44925b54580c5215f479cc95305e5c41550b446b
Erick Malta <erickmalta100@gmail.com>
2020-04-21 02:42:42 -0300

    Added a file to feed my Projects page, so it'll be easier to add more projects without having to change the base code

---
+++ src/services/feedProjects.js
@@ -0,0 +1,36 @@
+//Lógica dessa área é inspirada num projeto do usuário vmnog do github
+//https://github.com/vmnog
+
+const vectorProjects = [
+    {
+        id: 0,
+        name: 'test',
+        description: 'Descrição',
+        year: '2019'
+    },
+    {
+        id: 1,
+        name: 'test2',
+        description: 'Descrição 2',
+        year: '2019'
+    },
+    {
+        id: 2,
+        name: 'test3',
+        description: 'Descrição 3',
+        year: '2020'
+    },
+    {
+        id: 3,
+        name: 'test4',
+        description: 'Descrição 4',
+        year: '2020'
+    },
+    {
+        id: 4,
+        name: 'test5',
+        description: 'Descrição 5',
+        year: '2020'
+    }];
+
+module.exports = vectorProjects;
