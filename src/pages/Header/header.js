6e889a01d4459ea7dcae900f58bdb1fdb7af7682
Erick Malta <erickmalta100@gmail.com>
2020-04-21 02:40:31 -0300

    Added new classNames to fix some problems with global css use

--- src/pages/Header/header.js
+++ src/pages/Header/header.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { FaHome,FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa';
-import './styles.css';
+import { Link } from 'react-router-dom'
+import { FaHome,FaGithub, FaLinkedin, FaFacebook, FaTasks } from 'react-icons/fa';
+
 
 const Header = () => {
     return (
         <header>
             <nav>
-                <a id="home-button" href="/"><FaHome /><span>Início</span></a>
-                <ul>
+                <div className="home-button">
+                    <Link to="/"><FaHome /><span>Home</span></Link>
+                    <Link to="/projects"><FaTasks /><span>Projects</span></Link>
+                </div>
+                <ul className="nav-social-links">
                     <li><a href="https://github.com/ElladanTasartir"><FaGithub /><span>Github</span></a></li>
                     <li><a href="https://www.linkedin.com/in/erick-malta-8597a1197/"><FaLinkedin /><span>LinkedIn</span></a></li>
                     <li><a href="https://www.facebook.com/erick.demori"><FaFacebook /><span>Facebook</span></a></li>
