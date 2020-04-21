9c9e3fc66d55b6b29b5566c99dd88711528ee650
Erick Malta <erickmalta100@gmail.com>
2020-04-21 02:41:47 -0300

    Added a routes.js to work with routing

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import About from './pages/About/about';
+import Projects from './pages/Projects/projects';
+
+export default function Routes() {
+    return (
+            <Switch>
+                <Route path="/" exact component={About} />
+                <Route path="/projects" component={Projects} />
+            </Switch>
+    );
+}
