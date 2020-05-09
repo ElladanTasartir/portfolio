de6bf2ed93c55e71f648512ce6533b960df86f75
Erick Malta <erickmalta100@gmail.com>
2020-05-09 12:41:49 -0300

    Changed a bit of the text in the About section

--- src/pages/About/about.js
+++ src/pages/About/about.js
@@ -13,9 +13,8 @@ const About = () => {
                 <div id="container-text">
                     <h1 id="title">About:</h1>  
                     <p id="text">Hi! My name is Erick Demori Malta, a student aspiring to become a FullStack developer
-                    someday. I live in Brazil and I'm currently studying JavaScript and some of its libraries, like JQuery, 
-                    and frameworks, such as React, hence the reason I made this portfolio using it. 
-                    In any case, I have some experience
+                    someday. I live in Brazil and I'm currently studying JavaScript and some of its libraries, like JQuery and React, 
+                    hence the reason I made this portfolio using them. In any case, I have some experience
                     with C# and am a bit knowledgeable about Python and Java.</p>
                 </div>
                 <div id="container-img">
